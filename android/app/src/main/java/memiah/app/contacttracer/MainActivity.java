e9ef6121cd44c39d856536949a0923b29a32a737
Carlos Morillo <carlos@ehidra.com>
2020-03-27 15:51:13 +0100

    signup firebase test

--- android/app/src/main/java/memiah/app/contacttracer/MainActivity.java
+++ android/app/src/main/java/memiah/app/contacttracer/MainActivity.java
@@ -6,7 +6,7 @@ import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
 
 import java.util.ArrayList;
-
+import com.baumblatt.capacitor.firebase.auth.CapacitorFirebaseAuth;
 public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -16,6 +16,7 @@ public class MainActivity extends BridgeActivity {
     this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
+       add(CapacitorFirebaseAuth.class);
     }});
   }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'signup',
+    loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
+  },
+
 ];
 
 @NgModule({
