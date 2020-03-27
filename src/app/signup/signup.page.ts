f304edb33b9c3460be07302fe7e5a5dce50e544c
Carlos Morillo <carlos@ehidra.com>
2020-03-27 17:06:02 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	capacitor.config.json

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NavController} from '@ionic/angular';
+import {cfaSignIn} from 'capacitor-firebase-auth';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.page.html',
-  styleUrls: ['./signup.page.scss'],
+    selector: 'app-signup',
+    templateUrl: './signup.page.html',
+    styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
 
-  constructor() { }
+    public signupForm: FormGroup;
+    public submitAttempt = false;
 
-  ngOnInit() {
-  }
+    constructor(
+        public formBuilder: FormBuilder,
+        private navController: NavController,
+    ) {
+        this.signupForm = formBuilder.group({
+            phone: ['', Validators.compose([Validators.required])]
+        });
+    }
 
+    ngOnInit() {
+    }
+
+    validatePhone(phone) {
+
+        this.submitAttempt = true;
+        if (this.signupForm.valid) {
+            const newUser = {
+                phone: this.signupForm.value.phone
+            };
+            cfaSignIn('phone', {phone}).subscribe(
+                (user) => {
+                    console.log(user.phoneNumber);
+                }
+            );
+        }
+
+    }
 }
