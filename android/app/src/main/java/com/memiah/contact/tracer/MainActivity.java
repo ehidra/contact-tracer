85795fc8bc5651c2528d8f2426084f966db7dd4f
Carlos Morillo <carlos@ehidra.com>
2020-03-28 00:51:59 +0100

    android X kills me

--- android/app/src/main/java/com/memiah/contact/tracer/MainActivity.java
+++ android/app/src/main/java/com/memiah/contact/tracer/MainActivity.java
@@ -1,5 +1,5 @@
 package com.memiah.contact.tracer;
-
+import com.baumblatt.capacitor.firebase.auth.CapacitorFirebaseAuth;
 import android.os.Bundle;
 
 import com.getcapacitor.BridgeActivity;
@@ -16,6 +16,7 @@ public class MainActivity extends BridgeActivity {
     this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
+        add(CapacitorFirebaseAuth.class);
     }});
   }
 }
